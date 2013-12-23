1118fad1ff1f0c0ff680dc40caf905e0e7aa494c
Justin Buchanan <justbuchanan@gmail.com>
2013-12-22 23:52:21 -0500

    got DSL up and running!

--- lib/projsync.rb
+++ lib/projsync.rb
@@ -6,18 +6,20 @@ module Projsync
 
 	class Project
 		attr_accessor :group
+		attr_accessor :name
 
 
 		# the block passed in is run when the project syncs
 		# options:
 		# * origin
 		def initialize(group, name, options = {}, &block)
+			@name = name
 			@group = group
 			@path = options[:path] || name
 			@origin = options[:origin]
 			@sync_block = block
 		end
-		
+
 
 		def repo_path
 			fp = self.path
@@ -50,6 +52,10 @@ module Projsync
 			end
 		end
 
+
+		def inspect(indent = 0)
+			"#{'  ' * indent}Project: '#{self.name}'\n"
+		end
 	end
 
 
@@ -59,18 +65,51 @@ module Projsync
 		attr_accessor :subgroups
 		attr_accessor :projects
 
+		attr_accessor :name
 		attr_accessor :default
 
 
 		def initialize(parent, name, options = {})
+			options = {
+				default: true,
+				path: name
+			}.merge(options)
+
+			@name = name
 			@parent = parent
 			@default = options[:default]
-			@path = options[:path] || name
+			@path = options[:path]
+		end
+
+
+		def subgroups
+			@subgroups ||= []
 		end
 
 
 		def projects
 			@projects ||= []
+		end
+
+
+		def inspect(indent = 0)
+			i = '  ' * indent
+			desc = ""
+			desc += "#{i}Group: '#{self.name}'\n"
+
+			if self.projects.length > 0
+				desc = self.projects.inject(desc) do |desc, proj|
+					desc + proj.inspect(indent + 1)
+				end
+			end
+
+			if self.subgroups.length > 0
+				desc = self.subgroups.inject(desc) do |desc, group|
+					desc + group.inspect(indent + 1)
+				end
+			end
+
+			desc
 		end
 	end
 
@@ -78,7 +117,7 @@ module Projsync
 
 	class Manifest
 		def initialize
-			@tree = Group.new(nil, default: true)	# root group
+			@tree = Group.new(nil, nil, default: true)	# root group
 			@top_group = @tree
 		end
 
@@ -98,5 +137,16 @@ module Projsync
 			@top_group.projects << Project.new(@top_group, path, options, &block)
 		end
 
+
+		def sync()
+
+		end
+
+
+		def inspect
+			@tree.subgroups.inject("") do |desc, group|
+				desc + group.inspect(1)
+			end
+		end
 	end
 end
